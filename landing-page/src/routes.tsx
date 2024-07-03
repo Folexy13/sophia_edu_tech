b88590fe575b72955eeac7972bc9e2fa84d8f6fa
Foley13 <folajimiopeyemisax13@gmail.com>
2024-07-03 23:25:15 +0100

    [FEAT]:Quick changes...

--- landing-page/src/routes.tsx
+++ landing-page/src/routes.tsx
@@ -3,11 +3,8 @@ import { APPCONSTANTS } from "./constants";
 import {
 	Aboutpage,
 	ContactPage,
-	Forgotpage,
 	Homepage,
-	Loginpage,
 	Privacypage,
-	Registerpage,
 	SetNewPasswordPage,
 	Termspage,
 } from "./pages";
@@ -17,12 +14,9 @@ const Router: React.FC = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path={APPCONSTANTS.ROUTES[0].path} element={<Homepage />} />
-				<Route path={APPCONSTANTS.ROUTES[1].path} element={<Loginpage />} />
-				<Route path={APPCONSTANTS.ROUTES[2].path} element={<Registerpage />} />
 				<Route path={APPCONSTANTS.ROUTES[3].path} element={<Aboutpage />} />
 				<Route path={APPCONSTANTS.ROUTES[4].path} element={<Privacypage />} />
 				<Route path={APPCONSTANTS.ROUTES[5].path} element={<Termspage />} />
-				<Route path={APPCONSTANTS.ROUTES[6].path} element={<Forgotpage />} />
 				<Route
 					path={APPCONSTANTS.ROUTES[7].path}
 					element={<SetNewPasswordPage />}
