1cb1284588ae638aa94d618263c81df6d55c4bb6
Foley13 <folajimiopeyemisax13@gmail.com>
2024-08-05 08:24:14 +0100

    ...

--- main-app/vite.config.ts
+++ main-app/vite.config.ts
@@ -21,4 +21,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "dist", // or any other directory you choose
+	},
 });
