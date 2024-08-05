47c94eb3bf5e1e746f140651d5b9726c25e6e87f
Foley13 <folajimiopeyemisax13@gmail.com>
2024-08-05 07:56:24 +0100

    ...

--- main-app/vite.config.ts
+++ main-app/vite.config.ts
@@ -1,16 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	root: "./main-app", // Assuming main-app is your working directory
+// 	build: {
+// 		outDir: "dist", // Output directory
+// 		// By default, Vite expects index.html in the root of the project, if your structure is different adjust accordingly
+// 		// rollupOptions: {
+// 		// 	input: {
+// 		// 		main: "src/main.tsx", // Path to your main entry file if not using index.html
+// 		// 	},
+// 		// },
+// 	},
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	root: "./main-app", // Assuming main-app is your working directory
-	build: {
-		outDir: "dist", // Output directory
-		// By default, Vite expects index.html in the root of the project, if your structure is different adjust accordingly
-		rollupOptions: {
-			input: {
-				main: "index.html", // Path to your main entry file if not using index.html
-			},
-		},
-	},
 });
